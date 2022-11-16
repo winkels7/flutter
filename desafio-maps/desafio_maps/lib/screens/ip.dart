a01a7fcc60241d5ea3dda90787a30d61e6422d26
Joao Winkelmann <hmaster.studio@gmail.com>
2022-11-16 10:00:53 -0300

    Pegando valores do form

--- desafio-maps/desafio_maps/lib/screens/ip.dart
+++ desafio-maps/desafio_maps/lib/screens/ip.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart';
 
 class IP extends StatefulWidget {
   IP({super.key});
@@ -10,6 +14,12 @@ class IP extends StatefulWidget {
 class _IPState extends State<IP> {
   TextEditingController ipController = TextEditingController();
   String ipValue = '';
+
+  final _formKey = GlobalKey<FormState>();
+
+  chamadaAPI(value) {
+    log('valor: $value');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +35,44 @@ class _IPState extends State<IP> {
             )),
         Container(
             margin: EdgeInsets.all(20),
-            child: TextField(
-              controller: ipController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'IP',
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                      // The validator receives the text that the user has entered.
+                      validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.contains(new RegExp(r'[A-Z]'))) {
+                      return 'Por gentileza, insira um IP';
+                    }
+                    return null;
+                  }, onChanged: (text) {
+                    setState(() {
+                      ipValue = text;
+                    });
+                  }),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false otherwise.
+                        if (_formKey.currentState!.validate()) {
+                          // If the form is valid, display a snackbar. In the real world,
+                          // you'd often call a server or save the information in a database.
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processando Dados')),
+                          );
+                          chamadaAPI(ipValue);
+                        }
+                      },
+                      child: const Text('Enviar'),
+                    ),
+                  ),
+                ],
               ),
-              onChanged: (text) {
-                setState(() {
-                  ipValue = text;
-                  //you can access ipController in its scope to get
-                  // the value of text entered as shown below
-                  //ipValue = ipController.text;
-                });
-              },
             )),
         Container(
           margin: EdgeInsets.all(20),
