f023c244075eef806aea3d4b1ebc3ed211ce7c62
Joao Winkelmann <hmaster.studio@gmail.com>
2022-11-17 11:53:59 -0300

    classe final

--- desafio-maps/desafio_maps/lib/api/ipclass.dart
+++ desafio-maps/desafio_maps/lib/api/ipclass.dart
@@ -1,19 +1,49 @@
 class Album {
   final String status;
+  final String country;
+  final String countryCode;
+  final String region;
+  final String regionName;
   final String city;
+  final String zip;
+  final double lat;
+  final double lon;
+  final String timezone;
   final String isp;
+  final String org;
+  final String query;
 
   const Album({
     required this.status,
+    required this.country,
+    required this.countryCode,
+    required this.region,
+    required this.regionName,
     required this.city,
+    required this.zip,
+    required this.lat,
+    required this.lon,
+    required this.timezone,
     required this.isp,
+    required this.org,
+    required this.query
   });
 
   factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
       status: json['status'],
+      country: json['country'],
+      countryCode: json['countryCode'],
+      region: json['region'],
+      regionName: json['regionName'],
       city: json['city'],
+      zip: json['zip'],
+      lat: json['lat'].toDouble(),
+      lon: json['lon'].toDouble(),
+      timezone: json['timezone'],
       isp: json['isp'],
+      org: json['org'],
+      query: json['query'],
     );
   }
 }
