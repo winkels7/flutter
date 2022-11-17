96c6e4f321a57d97ce6279a951ae11c8ce72ac1b
Joao Winkelmann <hmaster.studio@gmail.com>
2022-11-17 09:32:02 -0300

    tentei

--- desafio-maps/desafio_maps/lib/screens/ip.dart
+++ desafio-maps/desafio_maps/lib/screens/ip.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 
 // import 'package:/desafio_maps/api/http_service.dart';
 // import 'package:/desafio_maps/api/post_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:desafio_maps/api/ipclass.dart';
 
 class IP extends StatefulWidget {
   const IP({super.key});
@@ -19,21 +22,45 @@ class _IPState extends State<IP> {
 
   final _formKey = GlobalKey<FormState>();
 
-  //vars http
+  // ignore: non_constant_identifier_names
+  List<IpTimer> IpTimerFromJson(String str) =>
+      List<IpTimer>.from(json.decode(str).map((x) => IpTimer.fromJson(x)));
 
-  chamadaAPI(value) async {
-    log('valor recebido do form: $value');
-    var url = Uri.http('ip-api.com', '/json/$value');
-    var response = await http.post(url);
-    // var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
-    log('status: $response.statusCode');
-    log(response.body);
+  Future<List<IpTimer>> _chamadaAPI(value) async {
+    var url = 'http://ip-api.com/json/$value';
+    log(url);
+
+    List<IpTimer> result;
+
+    var response = await http.post(Uri.parse(url));
+
+    if (response.statusCode == HttpStatus.ok) {
+      result = IpTimerFromJson(response.body);
+      log('ok!');
+      log(response.body);
+    } else {
+      log('Algo deu errado');
+      result = IpTimerFromJson(response.body);
+      log(response.body);
+    }
+    return result;
   }
+
+  // chamadaAPI(value) async {
+  //   log('valor recebido do form: $value');
+  //   var url = Uri.http('ip-api.com', '/json/$value');
+  //   var response = await http.post(url);
+  //   // var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+  //   log('status: $response.statusCode');
+  //   log(response.body);
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: <Widget>[
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
       Container(
           margin: const EdgeInsets.all(20),
           child: const Text(
@@ -45,6 +72,7 @@ class _IPState extends State<IP> {
           child: Form(
             key: _formKey,
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextFormField(
@@ -73,7 +101,7 @@ class _IPState extends State<IP> {
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(content: Text('Processando Dados')),
                         );
-                        chamadaAPI(ipValue);
+                        _chamadaAPI(ipValue);
                       }
                     },
                     child: const Text('Enviar'),
@@ -82,10 +110,30 @@ class _IPState extends State<IP> {
               ],
             ),
           )),
-      Container(
-        margin: const EdgeInsets.all(20),
-        child: Text(ipValue),
-      ),
+      Scaffold(
+          body: FutureBuilder<List<IpTimer>>(
+              future: _chamadaAPI(ipValue),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<IpTimer>> snapshot) {
+                return snapshot.hasData
+                    ? ListView.builder(
+                        itemCount: 1,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            elevation: 5.0,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10.0),
+                              ),
+                            ),
+                            color: Colors.white,
+                            margin: const EdgeInsets.all(0),
+                            child: Text(snapshot.data![index].city),
+                          );
+                        },
+                      )
+                    : const Text('Nao tem info ainda');
+              })),
     ]));
   }
 }
